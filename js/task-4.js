87985377f7401277affc9afe454108d78bd9443a
Oleh Kinchevskyi <Oleh.Kinchevskyi@gmail.com>
2020-09-19 23:03:02 +0300

    Додає ДЗ модуль 5

---
+++ js/task-4.js
@@ -0,0 +1,33 @@
+//переиспользование методов класса
+// Write code under this line
+class StringBuilder {
+  constructor(_value) {
+    this._value = _value;
+  }
+  get value() {
+    return this._value;
+  }
+  append(str) {
+    return (this._value += str);
+  }
+  prepend(str) {
+    this._value = str + this._value;
+  }
+  pad(str) {
+    this.append(str) + this.prepend(str);
+  }
+}
+
+// console.log(typeof StringBuilder);
+// 'function'
+
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
